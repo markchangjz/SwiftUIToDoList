e25be7b9b09888a66b4bf5c98b05758a62207a5d
Mark Chang <shea4a71@gmail.com>
2026-03-10 16:34:33 +0800

    Refactor SearchBar to use SwiftUI View instead of UIViewRepresentable, enhancing functionality with text field and cancel button.

--- SwiftUIToDoList/View/SearchBar.swift
+++ SwiftUIToDoList/View/SearchBar.swift
@@ -7,28 +7,86 @@
 
 import SwiftUI
 
-struct SearchBar: UIViewRepresentable {
+//struct SearchBar: UIViewRepresentable {
+//    @Binding var text: String
+//    
+//    func makeUIView(context: Context) -> some UIView {
+//        let searchBar = UISearchBar()
+//        searchBar.searchBarStyle = .minimal
+//        searchBar.autocapitalizationType = .none
+//        searchBar.placeholder = "Search..."
+//        searchBar.delegate = context.coordinator
+//        return searchBar
+//    }
+//    
+//    func updateUIView(_ uiView: UIViewType, context: Context) {
+//        if let searchBar = uiView as? UISearchBar {
+//            searchBar.text = text
+//        }
+//    }
+//    
+//    func makeCoordinator() -> Coordinator {
+//        Coordinator($text)
+//    }
+//
+//}
+
+// 客製化 SearchBar
+struct SearchBar: View {
     @Binding var text: String
-    
-    func makeUIView(context: Context) -> some UIView {
-        let searchBar = UISearchBar()
-        searchBar.searchBarStyle = .minimal
-        searchBar.autocapitalizationType = .none
-        searchBar.placeholder = "Search..."
-        searchBar.delegate = context.coordinator
-        return searchBar
-    }
-    
-    func updateUIView(_ uiView: UIViewType, context: Context) {
-        if let searchBar = uiView as? UISearchBar {
-            searchBar.text = text
+
+    @State private var isEditing = false
+    @FocusState private var isTextFieldFocused: Bool
+
+    var body: some View {
+        HStack {
+
+            TextField("Search ...", text: $text)
+                .textInputAutocapitalization(.never)
+                .autocorrectionDisabled(true)
+                .focused($isTextFieldFocused)
+                .padding(7)
+                .padding(.horizontal, 25)
+                .background(Color(.systemGray6))
+                .cornerRadius(8)
+                .overlay(
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                            .foregroundStyle(.gray)
+                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .leading)
+                            .padding(.leading, 8)
+
+                        if isEditing {
+                            Button(action: {
+                                self.text = ""
+                            }) {
+                                Image(systemName: "multiply.circle.fill")
+                                    .foregroundStyle(.gray)
+                                    .padding(.trailing, 8)
+                            }
+                        }
+                    }
+                )
+                .padding(.horizontal, 10)
+                .onChange(of: isTextFieldFocused) { _, newValue in
+                    withAnimation {
+                        isEditing = newValue
+                    }
+                }
+
+            if isEditing {
+                Button(action: {
+                    isEditing = false
+                    text = ""
+                    isTextFieldFocused = false
+                }) {
+                    Text("Cancel")
+                }
+                .padding(.trailing, 10)
+                .transition(.move(edge: .trailing))
+            }
         }
     }
-    
-    func makeCoordinator() -> Coordinator {
-        Coordinator($text)
-    }
-
 }
 
 #Preview {
